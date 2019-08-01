d0b61abaa158d74f9ff120c2d024f025a68b63dd
Filipe Moreno <y8.filipe@gmail.com>
2019-08-01 15:47:23 -0300

    Mapeamento de Histórico

--- src/main/java/com/db1/conta/contaapi/domain/entity/Historico.java
+++ src/main/java/com/db1/conta/contaapi/domain/entity/Historico.java
@@ -2,44 +2,44 @@ package com.db1.conta.contaapi.domain.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
 import org.springframework.util.Assert;
 
+@Embeddable
 public class Historico {
 	
 	public static final String VALOR_RESULTANTE_OBRIGATORIO = "Valor resultante é obrigatório";
 	public static final String CONTA_OBRIGATORIA = "Conta é obrigatória";
 	public static final String VALOR_OBRIGATORIO = "Valor é obrigatório";
 	public static final String TIPO_HISTORICO_OBRIGATORIO = "Tipo do histórico é obrigatório";
-	private Long id;
+
+	@Column(name="tipo")
 	private HistoricoTipo tipo;
+	
+	@Column(name="data")
 	private LocalDateTime data;
+	
+	@Column(name="valor")
 	private Double valor;
-	private Conta conta;
+
+	@Column(name="valor_resultante")
 	private Double valorResultante;
 	
-	public Historico(HistoricoTipo tipo, Double valor, Conta conta, Double valorResultante) {
+	public Historico(HistoricoTipo tipo, Double valor, Double valorResultante) {
 		Assert.notNull(tipo, TIPO_HISTORICO_OBRIGATORIO);
 		Assert.notNull(valor, VALOR_OBRIGATORIO);
-		Assert.notNull(conta, CONTA_OBRIGATORIA);
 		Assert.notNull(valorResultante, VALOR_RESULTANTE_OBRIGATORIO);
 		
 		this.tipo = tipo;
 		this.valor = valor;
-		this.conta = conta;
 		this.valorResultante = valorResultante;
 		
 	}
 	
- 	public Conta getConta() {
-		return conta;
-	}
-	
 	public LocalDateTime getData() {
 		return data;
-	}
-	
-	public Long getId() {
-		return id;
 	}
 	
 	public HistoricoTipo getTipo() {
